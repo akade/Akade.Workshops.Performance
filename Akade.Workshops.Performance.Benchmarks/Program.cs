d4d6587b440d553b12e3c8e5721d59dbfa017281
akade <16369295+akade@users.noreply.github.com>
2023-08-21 20:42:48 +0200

    Run

--- Akade.Workshops.Performance.Benchmarks/Program.cs
+++ Akade.Workshops.Performance.Benchmarks/Program.cs
@@ -3,16 +3,16 @@ using Akade.Workshops.Performance.Benchmarks.Introductory;
 using BenchmarkDotNet.Running;
 
 // Introductory
-BenchmarkSwitcher.FromTypes(new[] { typeof(NETUpdatesLinq), typeof(NETUpdatesStringOperations), typeof(EFCoreUpdatesInsertion), typeof(EFCoreUpdatesReading) });
+BenchmarkSwitcher.FromTypes(new[] { typeof(NETUpdatesLinq), typeof(NETUpdatesStringOperations), typeof(EFCoreUpdatesInsertion), typeof(EFCoreUpdatesReading) }).Run(args);
 
 // "Required"
-// BenchmarkSwitcher.FromTypes(new[] { typeof(EFCoreQueries), typeof(ExceptionsAsControlFlow), typeof(LazyLinqEnumeration), typeof(QuadraticScaling) });
+// BenchmarkSwitcher.FromTypes(new[] { typeof(EFCoreQueries), typeof(ExceptionsAsControlFlow), typeof(LazyLinqEnumeration), typeof(QuadraticScaling) }).Run(args);
 
 // "Intermediate"
-// BenchmarkSwitcher.FromTypes(new[] { typeof(EFCoreTracking), typeof(EmptyCollections), typeof(EnumParsing), typeof(PrematureOptimization), typeof(Serialization), typeof(StringComparisonBenchmarks), typeof(StringConcatenations) });
+// BenchmarkSwitcher.FromTypes(new[] { typeof(EFCoreTracking), typeof(EmptyCollections), typeof(EnumParsing), typeof(PrematureOptimization), typeof(Serialization), typeof(StringComparisonBenchmarks), typeof(StringConcatenations) }).Run(args);
 
 // "Advanced"
-// BenchmarkSwitcher.FromTypes(new[] { typeof(AdvancedStringParsing), typeof(BoxingAndSpecialization), typeof(BranchPrediction), typeof(CpuCache), typeof(Intrinsics) });
+// BenchmarkSwitcher.FromTypes(new[] { typeof(AdvancedStringParsing), typeof(BoxingAndSpecialization), typeof(BranchPrediction), typeof(CpuCache), typeof(Intrinsics) }).Run(args);
 
 // Normally you would do this:
 // BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args);
