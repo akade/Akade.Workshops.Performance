4a3996c8a9ae36f72b704488779bb2285da755e6
akade <16369295+akade@users.noreply.github.com>
2023-08-21 20:41:35 +0200

    Program cs

--- Akade.Workshops.Performance.Benchmarks/Program.cs
+++ Akade.Workshops.Performance.Benchmarks/Program.cs
@@ -1,7 +1,18 @@
 ﻿global using Akade.Workshops.Performance.Benchmarks.Infrastrucuture;
+using Akade.Workshops.Performance.Benchmarks.Introductory;
 using BenchmarkDotNet.Running;
 
-// TODO: init with types. I.e. Intro, Required, Intermediate & Advanced
-BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args);
+// Introductory
+BenchmarkSwitcher.FromTypes(new[] { typeof(NETUpdatesLinq), typeof(NETUpdatesStringOperations), typeof(EFCoreUpdatesInsertion), typeof(EFCoreUpdatesReading) });
 
+// "Required"
+// BenchmarkSwitcher.FromTypes(new[] { typeof(EFCoreQueries), typeof(ExceptionsAsControlFlow), typeof(LazyLinqEnumeration), typeof(QuadraticScaling) });
 
+// "Intermediate"
+// BenchmarkSwitcher.FromTypes(new[] { typeof(EFCoreTracking), typeof(EmptyCollections), typeof(EnumParsing), typeof(PrematureOptimization), typeof(Serialization), typeof(StringComparisonBenchmarks), typeof(StringConcatenations) });
+
+// "Advanced"
+// BenchmarkSwitcher.FromTypes(new[] { typeof(AdvancedStringParsing), typeof(BoxingAndSpecialization), typeof(BranchPrediction), typeof(CpuCache), typeof(Intrinsics) });
+
+// Normally you would do this:
+// BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args);
