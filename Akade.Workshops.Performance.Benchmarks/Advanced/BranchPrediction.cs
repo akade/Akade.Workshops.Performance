a9ecb24ac6aaf6faf6dcdf895d03a7d81eb99760
akade <16369295+akade@users.noreply.github.com>
2023-08-21 13:14:51 +0200

    Continued

--- Akade.Workshops.Performance.Benchmarks/Advanced/BranchPrediction.cs
+++ Akade.Workshops.Performance.Benchmarks/Advanced/BranchPrediction.cs
@@ -1,6 +1,50 @@
-﻿namespace Akade.Workshops.Performance.Benchmarks.Advanced
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Diagnosers;
+using BenchmarkDotNet.Diagnostics.Windows.Configs;
+
+namespace Akade.Workshops.Performance.Benchmarks.Advanced;
+
+[HardwareCounters(HardwareCounter.BranchMispredictions, HardwareCounter.BranchInstructions)]
+[FastJob]
+public class BranchPrediction
 {
-    internal class BranchPrediction
+    private const int N = 10000;
+    private readonly int[] data;
+
+    public BranchPrediction()
     {
+        Random random = new(0);
+        data = new int[N];
+        for (int i = 0; i < N; i++)
+        {
+            data[i] = random.Next(256);
+        }
+    }
+
+    [Benchmark]
+    public int Branch()
+    {
+        int sum = 0;
+        for (int i = 0; i < N; i++)
+        {
+            if (data[i] >= 128)
+            {
+                sum += data[i];
+            }
+        }
+
+        return sum;
+    }
+
+    [Benchmark]
+    public int Branchless()
+    {
+        int sum = 0;
+        for (int i = 0; i < N; i++)
+        {
+            int t = (data[i] - 128) >> 31;
+            sum += ~t & data[i];
+        }
+        return sum;
     }
 }
--- Akade.Workshops.Performance.Benchmarks/Advanced/CpuCache.cs
+++ Akade.Workshops.Performance.Benchmarks/Advanced/CpuCache.cs
@@ -1,12 +1,134 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Diagnosers;
 
-namespace Akade.Workshops.Performance.Benchmarks.Advanced
+namespace Akade.Workshops.Performance.Benchmarks.Advanced;
+
+
+/// <summary>
+/// Multidimensional arrays are slower than jagged ones
+/// Jagged ones are slower than a single continous bit
+/// Continous memory access is faster than random (less cache misses)
+/// </summary>
+[HardwareCounters(HardwareCounter.CacheMisses)]
+[FastJob]
+public class CpuCache
 {
-    internal class CpuCache
+    private const int N = 1000;
+    public int[][] _values;
+    public int[,] _values2;
+    public int[] _values3;
+
+    public CpuCache()
     {
+        Random r = new(42);
+        _values = new int[N][];
+        _values2 = new int[N, N];
+        _values3 = new int[N * N];
+        for (int x = 0; x < N; x++)
+        {
+            _values[x] = new int[N];
+            for (int y = 0; y < N; y++)
+            {
+                _values3[(x * N) + y] = _values[x][y] = _values2[x, y] = r.Next();
+
+            }
+        }
     }
+
+    [Benchmark]
+    public long Sum()
+    {
+        long sum = 0;
+
+        for (int x = 0; x < N; x++)
+        {
+            for (int y = 0; y < N; y++)
+            {
+                sum += _values[x][y];
+            }
+        }
+
+        return sum;
+    }
+
+    [Benchmark]
+    public long Sum2()
+    {
+        long sum = 0;
+
+        for (int x = 0; x < N; x++)
+        {
+            for (int y = 0; y < N; y++)
+            {
+                sum += _values[y][x];
+            }
+        }
+
+        return sum;
+    }
+
+    [Benchmark]
+    public long Sum3()
+    {
+        long sum = 0;
+
+        for (int x = 0; x < N; x++)
+        {
+            for (int y = 0; y < N; y++)
+            {
+                sum += _values2[x, y];
+            }
+        }
+
+        return sum;
+    }
+
+    [Benchmark]
+    public long Sum4()
+    {
+        long sum = 0;
+
+        for (int x = 0; x < N; x++)
+        {
+            for (int y = 0; y < N; y++)
+            {
+                sum += _values2[y, x];
+            }
+        }
+
+        return sum;
+    }
+
+    [Benchmark]
+    public long Sum5()
+    {
+        long sum = 0;
+
+        for (int x = 0; x < N; x++)
+        {
+            for (int y = 0; y < N; y++)
+            {
+                sum += _values3[x * N + y];
+            }
+        }
+
+        return sum;
+    }
+
+    [Benchmark]
+    public long Sum6()
+    {
+        long sum = 0;
+
+        for (int x = 0; x < N; x++)
+        {
+            for (int y = 0; y < N; y++)
+            {
+                sum += _values3[y * N + x];
+            }
+        }
+
+        return sum;
+    }
+
 }
